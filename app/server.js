599840d0bee4d9e99dae40dfa8c2136bf8b1c1d5
Robert Stapleton <robert@stapleton-family.net>
2022-11-20 10:48:51 -0500

    server.js modifications to meet specifics used during lesson exercise

--- app/server.js
+++ app/server.js
@@ -21,19 +21,19 @@ app.get('/profile-picture', function (req, res) {
 });
 
 // use when starting application locally with node command
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+let mongoUrlLocal = "mongodb://admin:admin@localhost:27017";
 
 // use when starting application as a separate docker container
-let mongoUrlDocker = "mongodb://admin:password@host.docker.internal:27017";
+let mongoUrlDocker = "mongodb://admin:admin@host.docker.internal:27017";
 
 // use when starting application as docker container, part of docker-compose
-let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
+let mongoUrlDockerCompose = "mongodb://admin:admin@mongodb";
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
-let databaseName = "my-db";
+let databaseName = "user-account";
 
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
